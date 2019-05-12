320944c27aec0df431e87403c8905980f37f6e77
Ramandeep Kaur <raman80800@gmail.com>
2019-05-12 15:02:16 +0530

    Paypal Integration in android application

---
+++ app/src/main/java/com/example/paypalintegration/PaypalConfig.java
@@ -0,0 +1,5 @@
+package com.example.paypalintegration;
+
+public class PaypalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AcFNp1F4zUEkAgE-3vQl06ZP7ZNtEdCvgDflDKaGYvj0hmXhKvb8oXWrds0PR2L_xoQqcXMneGbQdP0x";
+}
